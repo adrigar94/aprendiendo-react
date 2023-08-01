dead56911867a506217db2e419fdc0b279212ceb
Adrián García <akrian231@gmail.com>
2023-08-01 11:49:14 +0200

    feat follow mouse

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react"
+
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: -100, y: -100 })
+
+  useEffect(() => {
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({ x: -100, y: -100 })
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  )
+}
+
+
 function App() {
 
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
